b2e6b408a66e858c9758abfb4d70b387fae16d8e
joydragon_wang <joydragon_wang@noreply.gitcode.com>
2025-11-29 17:21:46 +0800

    feat(swagger): 为简单响应装饰器添加ApiExtraModels自动注册
    添加ApiExtraModels装饰器以自动注册模型，确保Swagger文档能正确显示响应模型

--- src/api-simple-array-response.decorator.ts
+++ src/api-simple-array-response.decorator.ts
@@ -1,11 +1,12 @@
 import { applyDecorators, Type } from '@nestjs/common';
-import { ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
 
 import { SimpleResponseDto } from './simple-response.dto';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
 export const ApiSimpleArrayResponse = <TModel extends Type>(model: TModel) => {
   return applyDecorators(
+    ApiExtraModels(model), // 自动注册
     ApiOkResponse({
       schema: {
         allOf: [
